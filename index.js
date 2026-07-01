aa17948ca073dc9380af35b2bf1be8d6aeabc6f1
Abhavya28 <abhavya2801@gmail.com>
2026-07-01 18:14:36 +0530

    Implement basic CLI command parsing

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello URL SHORTNER CLI");
+// const command = process.argv[2];
+// const url = process.argv[3];
+
+// if (command === "shorten") {
+//   if (!url) {
+//     console.log("Please provide a URL.");
+//     return;
+//   }
+//   console.log("Shortening ", url);
+// } else if (command === "list") {
+//   console.log("Listing all urls");
+// } else if (command === "delete") {
+//   if (!url) {
+//     console.log("Please provide a short code.");
+//     return;
+//   }
+//   console.log("Deleting ", url);
+// } else {
+//   console.log("Unknow Command");
+// }
+
+
+const fs = require("fs");
+
+fs.readFile("urls.json", "utf8", (err,data) =>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(data);
+})
+
